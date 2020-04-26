75dab73aa56b7d6fbbdce4ece69bc7dd815d79c8
moemi ohtsu <s0623192@gmail.com>
2020-04-26 18:44:41 +0900

    add: コンフリクト解消

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
-=======
-
-
-
-Route::get('hello/{msg?}', function($msg='no message') {
-  $html = <<<EOF
-<html>
-  <head>
-    <title>Hello</title>
-    <style>
-      body { font-size: 16pt; color: #999; }
-      h1 { font-size: 100pt; text-align: right; color: #eee; margin: -40px 0px -50px 0px; }
-    </style>
-  </head>
-  <body>
-    <h1>Hello</h1>
-    <p>($msg)</p>
-    <p>これはサンプルで作ったページです。</p>
-  </body>
-</html>
-EOF;
-    return $html;
-});
->>>>>>> master
