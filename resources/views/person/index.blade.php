b68b56873a2cefde9e7a4c55ef0e48d243776cac
moemi ohtsu <s0623192@gmail.com>
2020-05-26 22:20:31 +0900

    add: personモデルをboardモデルと１対多の関係に

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -10,14 +10,22 @@
 @section('content')
     <table>
         <tr>
+            <th>Person</th>
             <th>Board</th>
         </tr>
         @foreach ($items as $item)
             <tr>
                 <td>{{ $item->getData() }}</td>
-                <td>@if ($item->board != null)
-                        {{$item->board->getData()}}
-                    @endif
+                <td>
+                @if ($item->boards != null)
+                    <table width="100%">
+                    @foreach ($item->boards as $obj)
+                        <tr>
+                            <td>{{$obj->getData()}}</td>
+                        </tr>
+                    @endforeach
+                    </table>
+                @endif
                 </td>
             </tr>
         @endforeach
