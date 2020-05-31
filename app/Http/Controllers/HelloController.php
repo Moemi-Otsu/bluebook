57bafa40d5233abedbd58f0cda9b540f6bde7461
moemi ohtsu <s0623192@gmail.com>
2020-05-31 16:46:48 +0900

    /hello/restのRESTful

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -82,4 +82,9 @@ class HelloController extends Controller
             ->where('id', $request->id)->delete();
         return redirect('/hello');
     }
+
+    public function rest(Request $request)
+    {
+        return view('hello.rest');
+    }
 }
