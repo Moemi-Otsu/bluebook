a0a2c0ced3f2ccac58137536895e6fc4f17b6610
moemi ohtsu <s0623192@gmail.com>
2020-06-15 23:48:13 +0900

    /helloでログインをチェックする

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,16 +8,18 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Requests\HelloRequest;
 use Validator;
 use App\Person;
+use Illuminate\Support\Facades\Auth;
 
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
+        $user = Auth::user();
         $sort = $request->sort;
         $items = Person::orderBy($sort, 'asc')
-            ->paginate(5);
-        $param = ['items' => $items, 'sort' => $sort];
+            ->simplePaginate(5);
+        $param = ['items' => $items, 'sort' => $sort, 'user' => $user];
         return view('hello.index', $param);
     }
 
