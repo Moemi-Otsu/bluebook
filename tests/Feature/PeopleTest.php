205ffeb4a1583cfb4f10aa5b96719a97af75f137
moemi ohtsu <s0623192@gmail.com>
2020-06-23 23:30:56 +0900

    helloのユニットテスト記述

--- tests/Feature/PeopleTest.php
+++ tests/Feature/PeopleTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class PeopleTest extends TestCase
 {
     /**
      * A basic test example.
